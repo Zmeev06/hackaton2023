04e8df1323c6bc6f4abdfc84586dc08c1db24307
NikitaTorbenko <2015nekitciti@gmail.com>
2023-03-17 21:28:17 +0300

    ff

--- src/componets/TrackList.jsx
+++ src/componets/TrackList.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import {Box, Container, Heading} from '@chakra-ui/react'
+import { Box, Container, Flex, IconButton, Heading } from '@chakra-ui/react';
+import firstSvg from '../assets/TrackList/1.svg'
 
 const TrackList = () => {
-    return ( 
-        <Box>
-            <Container>
-                <Heading>Track list</Heading>
-                <Flex></Flex>
-            </Container>
-        </Box>
-    );
-}
- 
+  return (
+    <Box>
+      <Container>
+        <Heading>Track list</Heading>
+        <Flex>
+          <IconButton
+          onClick={() => console.log('hui')}
+            colorScheme="blue"
+            aria-label="Search database"
+            icon={firstSvg}
+          />
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
+
 export default TrackList;
