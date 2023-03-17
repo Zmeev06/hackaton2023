4754f7746f1fcb774f22debfda5dbb7144c9a935
Zmeev06 <88145578+Zmeev06@users.noreply.github.com>
2023-03-17 22:44:47 +0300

    icons

--- src/componets/UI/Tabs.jsx
+++ src/componets/UI/Tabs.jsx
@@ -1,15 +1,18 @@
-import { Box, Container, Flex, IconButton } from '@chakra-ui/react';
+import { Avatar, Box, Container, Flex, IconButton } from '@chakra-ui/react';
 import React, {useState} from 'react'
 import Headphones from './CustomIcons/Headphones'
+import LikeIcon from './CustomIcons/LikesIcon'
+import BooksIcon from './CustomIcons/BooksIcon'
 
 const Tabs = () => {
-    const [btns, setBtns] = useState([{img:"./assets/headfones.svg"}, {img:"./assets/like.svg"}, {img:"./assets/books.svg"}])
+    const [btns, setBtns] = useState([{img:<Headphones color={"white"}/>}, {img:<LikeIcon color={"white"}/>}, {img:<BooksIcon color={"white"}/>}])
     return (
-        <Container backgroundColor={"#F2F2F2"} maxW={"65"} height={"100vh"}>
-            <Flex flexDirection={"column"} maxW="50px" justifyContent={'space-around'}>
-                {btns.map((btn, index) => <IconButton colorScheme={"white"} icon={<Headphones></Headphones>} key={index}></IconButton>)}
+        <Flex backgroundColor={"#F2F2F2"} maxW={"65"} height={"100vh"} flexDirection={"column"} width={"60px"} alignItems={"center"} py={6} >
+            <Avatar src='./assets/tabsicons/profile.jpg'/>
+            <Flex flexDirection={"column"} maxW="50px" justifyContent={'space-around'} alignItems={"center"} height={"150px"}>
+                {btns.map((btn, index) => <IconButton borderRadius={20} colorScheme={"pink"} icon={btn.img} key={index}/>)}
             </Flex>
-        </Container>
+        </Flex>
      );
 }
  
